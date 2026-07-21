8e9a73139afa5a1928a6ff62bc6f9f48ac3e9a65
VIBIN RICKY IR <727824tuam053@skct.edu.in>
2026-07-21 14:14:19 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/GraphController.java
+++ backend/src/main/java/com/example/demo/controller/GraphController.java
@@ -64,7 +64,7 @@ public class GraphController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    // @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<KnowledgeGraph> createGraph(@Valid @RequestBody KnowledgeGraph graph) {
         KnowledgeGraph created = graphOrchestratorService.createGraph(graph);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
